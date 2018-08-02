9ad5e697674708659b2afac1d221e82a31dfe437
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-02 19:55:08 +0000

    updated order_controller new

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,19 @@
 class OrdersController < ApplicationController
   def new
-    @order = Order.new(user_id: params[:user_id])
+    if order_authorize
+      @order = Order.new(user_id: @user.id)
+    else
+      return head(:forbidden)
+    end
+  end
+
+  private
+
+  def order_authorize
+    if session[:user_id] == params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+    else
+      return nil
+    end
   end
 end
