95ce689f75d691588927f74f64eef1ee6b73d916
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-02 15:48:03 +0000

    added admin show in routes & controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @post.valid?
       @user.save
-      redirect_to user_path(@user)
+      session[:user_id] = @user.id
+      if !@user.admin
+        redirect_to user_path(@user)
+      else
+        redirect_to admin_user
     else
       render :new
     end
+  end
+
+  def admin_show
+    return head(:forbidden) unless current_user.try(:admin)
+    find_user
+  end
+
+  def show
+    current_user ? find_user : redirect_to root_path
   end
 
 
@@ -19,5 +32,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :phone_number, :admin)
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
   end
 end
