5b6e47802a329440b68d9ec02602388f6a88c415
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-02 15:27:40 +0000

    added create in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
+  def create #need to check if validation errors are being diisplayed in the form?
+    @user = User.new(user_params)
+    if @post.valid?
+      @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :phone_number, :admin)
   end
 end
