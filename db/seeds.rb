eabb1054d405c4a4e3036b743bf020ecd851124e
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-02 14:26:12 +0000

    updated db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user1 = User.create(name: "Mickey", password: "mouse", email: "mickey@gmail.com")
+user1 = User.create(name: "Mickey", password: "mouse", email: "mickey@gmail.com", phone_number: "1234567890")
 product1 = Product.create(name: "Mickey's Magic Gloves", price: 10.99, inventory: 100)
 order1 = Order.create(user_id: 1)
 item1 = Item.create(user_id: 1, product_id: 1, order_id: 1)
