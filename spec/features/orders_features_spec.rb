8dbe4e320409bd4696d0c58f05be9b6971247286
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-03 20:12:59 +0000

    updated order features spec

--- spec/features/orders_features_spec.rb
+++ spec/features/orders_features_spec.rb
@@ -7,12 +7,27 @@ describe "order creation", type: :feature do
     expect(current_path).to eq('/')
   end
 
-  it "contains products form" do
+  it "creates eligible orders after submission" do
+
+  end
+end
+
+describe "orders index", type: :feature do
+  it "only allows admin users to access page" do
 
   end
 
-  it "only creates eligible orders after submission" do
+  it "allows admin users to fulfill order" do
+
+  end
+end
+
+describe "orders show", type: :feature do
+  it "only allows admin users or users who created the order to view" do
 
   end
 
+  it "contains link to be edited or deleted" do
+
+  end
 end
