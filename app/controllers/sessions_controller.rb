1220774bae2b366a362b40b21550d935a08f01ab
github username <github email address>
2018-08-04 12:32:13 -0400

    minor update to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
   def new
+    if current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def create
@@ -11,5 +14,9 @@ class SessionsController < ApplicationController
       flash[:error] = "Authentication Failed"
       render 'new'
     end
+  end
+
+  def delete
+    if current_user
   end
 end
