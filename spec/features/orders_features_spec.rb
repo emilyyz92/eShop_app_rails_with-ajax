41060f582fc9ce20965bd07458ca06e61014cf5d
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-06 16:49:33 +0000

    updates to order features spec

--- spec/features/orders_features_spec.rb
+++ spec/features/orders_features_spec.rb
@@ -1,6 +1,12 @@
 require_relative "../rails_helper.rb"
 
+given(:harry) {User.make(name: "Harry Potter", email: "hp@hogwarts.com", password: "scar", phone: "1111111111")}
+given(:order1) {Order.make(user_id: harry.id)}
+given(:headmaster) {User.make(name: "Dumbledore", email: "albus@hogwarts.com", password: "golden snitch", phone: "2222222222", admin: true)}
+given(:malfoy) {User.make(name: "Malfoy", email: "malfoy@hogwarts.com", password: "Dolby", phone: "3333333333")}
+
 describe "order creation", type: :feature do
+
   it "only allows users to access page if logged in" do
     page.set_rack_session(user_id: nil)
     visit "/users/1/orders/new"
@@ -19,25 +25,23 @@ end
 
 describe "orders index", type: :feature do
   it "only allows admin users to access page" do
-    @minnie = User.create(
-      name: "Minnie",
-      password: "password",
-      email: "minnie@disney.com",
-      phone: "9999999999"
-      )
-    page.set_rack_session(user_id: @minnie.id)
+    page.set_rack_session(user_id: malfoy.id)
+    visit orders_path
     expect(content).to have_content("You don't have access")
   end
 
   it "links to order show page" do
-    click_button "Fulfill Order #1"
-
+    page.set_rack_session(user_id: headmaster.id)
+    visit orders_path
+    expect(content).to have_content("Order 1")
   end
 end
 
 describe "orders show", type: :feature do
   it "only allows admin users or users who created the order to view" do
-
+    page.set_rack_session(user_id: malfoy.id)
+    visit order_path(order1)
+    expect(current_path).to eq(user_path(malfoy))
   end
 
   it "contains link to be edited or deleted" do
