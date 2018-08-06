e7e27de033977c727d197de856aea95cde8a20dd
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-06 17:15:33 +0000

    refactored orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,11 @@
 class OrdersController < ApplicationController
   def new
-    if order_authorize
+    if authorized_user
       @order = Order.new(user_id: @user.id)
       @products = Product.all
     else
       return head(:forbidden)
+      redirect_to root_path
     end
   end
 
@@ -12,20 +13,45 @@ class OrdersController < ApplicationController
   end
 
   def edit
-    if order_authorize
+    if authorized_user
       @order = Order.find_by(id: params[:id])
     else
       return head(:forbidden)
     end
   end
 
-  private
-
-  def order_authorize
-    if session[:user_id] == params[:user_id]
-      @user = User.find_by(id: params[:user_id])
+  def index
+    if authorized_user
+      @orders = Order.where(user_id: params[:user_id])
+    elsif admin_user?
+      @orders = Order.all
     else
-      return nil
+      redirect_to '/'
     end
   end
+
+  def delete
+    if authorized_user || admin_user
+      find_order
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+
+  def authorized_user
+    if session[:user_id] == params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+    end
+  end
+
+  def find_order
+    @order = Order.find_by(id: params[:id])
+  end
+
+  def admin_user?
+    User.find_by(id: session[:user_id]).admin
+  end
+
 end
