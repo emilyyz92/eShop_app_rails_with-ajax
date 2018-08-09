ce7bb0e6aa30ea634c287f0edf9659cafbf7b1f2
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-09 20:47:07 +0000

    added new product features tests

---
+++ spec/features/product_features_spec.rb
@@ -0,0 +1,37 @@
+require_relative "../rails_helper.rb"
+
+describe "product", type: :feature do
+  order_helper
+  it "allows admin users to successfully create a product" do
+    page.set_rack_session(user_id: headmaster.id)
+    visit new_product_path
+    create_product
+    expect(current_path).to eq('/products/1')
+  end
+
+  it "does not allow non-user or standard user to create product" do
+    page.set_rack_session(user_id: nil)
+    visit new_product_path
+    expect(current_path).to eq(products_path)
+  end
+
+  it "does allows admin users to edit and delete product" do
+    page.set_rack_session(user_id: headmaster.id)
+    visit products_path
+    click_link('The Elder Wand')
+    expect(page).to have_button("Edit Product")
+    click_link("Delete Product")
+    expect(current_path).to eq(products_path)
+  end
+
+  it "allows admin user to successfully edit product" do
+
+  end
+
+  it "shows admin users all orders of the product on its show page" do
+
+  end
+
+  it "shows standard users details of the product" do
+  end
+end
--- spec/support/helper.rb
+++ spec/support/helper.rb
@@ -3,4 +3,12 @@ def order_helper
   let(:order1) {Order.create(user_id: harry.id)}
   let(:headmaster) {User.create(name: "Dumbledore", email: "albus@hogwarts.com", password: "golden snitch", phone_number: "2222222222", admin: true)}
   let(:malfoy) {User.create(name: "Malfoy", email: "malfoy@hogwarts.com", password: "Dolby", phone_number: "3333333333")}
+  let(:product) {Product.create(name: "The Elder Wand", inventory: 1, price: 999.98)}
+end
+
+def create_product
+  fill_in('Name', with: "Nimbus 2000")
+  fill_in('Inventory', with: "200")
+  fill_in("Price", with: "29.99")
+  click_on "Create Product"
 end
