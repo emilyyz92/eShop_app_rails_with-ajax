5813de39593232009778a9856c7236ce6e32dd78
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-10 17:06:08 +0000

    added omniauth google features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,8 +16,24 @@ class SessionsController < ApplicationController
     end
   end
 
+  def create_google #for logging in through OmniAuth google
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+    end
+
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
   def delete
     session[:user_id] = nil
     redirect_to '/'
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :phone_number, length: {is: 10}
   validates :password, length: {minimum: 2, maximum: 50}
 
   has_many :orders
