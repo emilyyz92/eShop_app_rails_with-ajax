fdca2db47aa4549fff74bc07ae9c2512cef5b69c
github username <github email address>
2018-08-16 15:04:50 -0400

    added feature to destroy all user's orders before user deletes account

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: {minimum: 2, maximum: 50}, allow_blank: true
+  before_destroy :delete_orders
 
   has_many :orders
   has_many :items, through: :orders
@@ -11,5 +12,11 @@ class User < ApplicationRecord
 
   def uniq_products
     products.uniq {|product| product.id}
+  end
+
+  private
+
+  def delete_orders
+    Order.where(user_id: self.id).each {|order| order.delete}
   end
 end
