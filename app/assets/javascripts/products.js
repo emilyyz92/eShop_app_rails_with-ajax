1cd043fd59c0cc90dd904748656622cca3e9e732
github username <github email address>
2018-10-18 15:28:40 -0400

    refactored more details js code for using OO JS and create prototype functions

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -9,28 +9,51 @@ function moreDetails() {
       e.preventDefault();
       var productID = this.dataset.id
       fetch("/products/" + productID + ".json").then(resp => resp.json())
-      .then(function(product) {
+      .then(function(productResp) {
+        //create product javascript object
+        if (productResp.description) {
+          var product = new Product(productResp.name, productResp.id,
+            productResp.inventory, productResp.price, productResp.description)
+        } else {
+          var product = new Product(productResp.name, productResp.id, productResp.inventory,
+          productResp.price)
+        }
+        //expand product description
         if(product.description) {
           document.getElementById('description-' + productID).innerHTML =
           product.description
         }
-        if (product.inventory > 0) {
-          var availability = "In Stock"
-        } else {
-          var availability = "Temporarily Out of Stock"
-        }
+        //create element, and set attributes for availability and price
         var availItem = document.createElement("small")
         var priceItem = document.createElement("p")
         availItem.className = "card-text avail"
-        availItem.innerHTML = availability
         priceItem.className = "card-text price"
+        availItem.innerHTML = product.availability()
         priceItem.innerHTML = "Price: $" + product.price
         let cardBody = document.getElementById("body-" + productID)
         let productDetail = document.getElementById("details-" + productID)
+        //insert price and availability elements inside product card body
         cardBody.insertBefore(availItem, productDetail)
         cardBody.insertBefore(priceItem, productDetail)
+        //remove more details button
         cardBody.removeChild(productDetail)
       })
     })
+  }
+}
+
+function Product (name, id, inventory, price, description = "") {
+  this.name = name
+  this.id = id
+  this.description = description
+  this.inventory = inventory
+  this.price = price
+}
+
+Product.prototype.availability = function() {
+  if (this.inventory > 0) {
+    return "In Stock"
+  } else {
+    return "Temporarily Out of Stock"
   }
 }
