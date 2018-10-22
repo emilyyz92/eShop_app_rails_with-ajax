85ec814912a9881bfe1e13051a07ebde427af4c9
github username <github email address>
2018-10-21 22:25:06 -0400

    added AJAX button for edit phone and email for users account

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,11 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+  const jumbotron = document.querySelector(".jumbotron")
+  const userID = jumbotron.dataset.userid
+  deleteButton(userID)
+  editPhoneButton(jumbotron)
 })
 
-function deleteButton() {
-  const delete = document.getElementById('delete-account')
-  const userID = delete.dataset.userid
-  delete.addEventListener("click", function() {
+function deleteButton(userID) {
+  let delButton = document.getElementById('delete-account')
+  delButton.addEventListener("click", function() {
     fetch("/users/" + userID, {method: "DELETE"})
   })
+}
+
+function PhoneButton(jumbotron) {
+  const addPhone = document.getElementById("add-phone")
+  const editPhone = document.getElementById('edit-phone')
+  if (addPhone) {
+    addPhone.addEventListener("click", function() {
+      addPhoneForm(jumbotron, addPhone)})
+  } else {
+    editPhone.addEventListener("click", function() {
+      addPhoneForm(jumbotron, editPhone) })
+  }
+}
+
+function addPhoneForm(jumbotron, phone) {
+  var form = document.createElement("form")
+  var input = document.createElement("input")
+  var label = document.createElement("label")
+  var submit = document.createElement("input")
+  input.type = "text"
+  submit.type = "submit"
+  label.innerHTML = "Phone Number"
+  form.appendChild(label)
+  form.appendChild(input)
+  jumbotron.insertBefore(form, phone)
 }
