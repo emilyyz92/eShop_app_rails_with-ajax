0b0d365a9594dafcd81f2cf056f9f38383eb2b0f
github username <github email address>
2018-10-24 14:34:54 -0400

    fixed function bugs

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -8,21 +8,29 @@ function nextOrderButton() {
     var userID = button.dataset.userid
     var orderID = button.dataset.orderid
     var template = Handlebars.compile(document.getElementById("order-table-template").innerHTML)
-    //get JSON of next order's ID
+    //get JSON of the user's next order's ID
     fetch("/users/" + userID + "/orders/" + orderID).then(resp => resp.json()).then(function(orderResp) {
-      orderID = orderResp.next_order_id
+      if (orderResp.next_order_id) {
+        orderID = orderResp.next_order_id
+        //get next order's info in JSON
+        fetch("/users/" + userID + "/orders/" + orderID).then(resp => resp.json()).then(function(orderResp) {
+          var products = orderResp.products
+          var productWithUnits = orderResp.product_with_units
+          //clear table data
+          document.getElementById("table-data").innerHTML = ""
+          for(var i in products) {
+            var productData = {productID: products[i].id,
+              productName: products[i].name,
+              orderProductCount: productWithUnits[products[i].name],
+              productPrice: products[i].price
+            }
+            //update table content with new order data
+            document.getElementById("table-data").innerHTML += template(productData)
+          }
+        })
+      } else {alert("This is your last order.")}
     })
-    //get next order's info in JSON
-    fetch("/users/" + userID + "/orders/" + orderID).then(resp => resp.json()).then(function(orderResp) {
-      var products = orderResp.products
-      var productWithUnits = orderResp.product_with_units
-      for(var product in products) {
-        var productData = {productID: product.id,
-          productName: product.name,
-          orderProductCount: productWithUnits[product.name],
-          productPrice: product.price
-        }
-      }
-    })
+    //reset button's order ID
+    button.setAttribute("data-orderid", orderID)
   })
 }
